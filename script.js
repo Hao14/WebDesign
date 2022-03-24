fe6ac4f2b0ee5e64179eb20740549e5b7c24eeee
Hao14 <45506744+Hao14@users.noreply.github.com>
2022-03-24 10:34:03 -0700

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const navSlide = () => {
-    const menu1 = document.querySelector('.menu1');
-    const nav = document.querySelector('.nav-links');
-
-    menu1.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-    });
-}
-
