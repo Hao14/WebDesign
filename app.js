7298fd1613f642460e2cd021bcf6c168415c88d9
itsrichardyu <deadlyhunter295@gmail.com>
2022-04-06 21:47:55 -0700

    Mobile navbar fix

--- app.js
+++ app.js
@@ -15,10 +15,12 @@ const navSlide = () => {
         });
         menu1.classList.toggle('toggle');
 
-        if (nav.style.width = "0%") {
-            nav.style.width = "100%"
+        if (window.innerWidth < 400 && nav.style.opacity == "1" && nav.style.width == "100%") {
+            nav.style.opacity = "0";
+            nav.style.width = "0%";
         } else {
-            nav.style.width = "0%"
+            nav.style.opacity = "1";
+            nav.style.width = "100%";
         }
         
         console.log(navLinks.width);
